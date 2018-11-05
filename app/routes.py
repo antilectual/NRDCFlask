848921fc6ce160a3b47ecd85cc23018cfc8d7610
antilectual <matthewwjohnson@nevada.unr.edu>
2018-11-04 16:36:40 -0800

    Prep for searching ontology

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from flask import jsonify
 #RDF importer
 import rdflib	
 
+#create triples graph			
+g=rdflib.Graph()
+#load ontology into triples
+g.parse("NRDCOntology.xml", format="xml")
+	
 # setting up the URIs for RESTful server. This is base URI
 @nrdcApp.route('/')							
 @nrdcApp.route('/index')
@@ -12,11 +17,6 @@ def index():
 # This is a special URI to retrieve the JSON tasks	
 @nrdcApp.route('/test', methods=['GET']) 
 def get_tasks():
-			
-	#create triples graph			
-	g=rdflib.Graph()
-	#load ontology into triples
-	g.parse("NRDCOntology.xml", format="xml")
 	tupleslist = []
 	#ID key
 	id = 0
@@ -28,4 +28,18 @@ def get_tasks():
 	#format and return triples
 	return jsonify(tupleslist)
 
-		
+	
+	
+# This is a special URI to retrieve the JSON tasks	
+#	@nrdcApp.route('/organizationTier', methods=['GET']) 
+#	def getOTs():
+#		tupleslist = []
+#		#ID key
+#		id = 0
+#		#for each triple in the graph
+#		for s,p,o in g.triples(None, None, None):
+#			newSPO = {'id': id, 'o': o, 'p': p, 's': s}
+#			id = id+1
+#			tupleslist.append(newSPO)
+#		#format and return triples
+#		return jsonify(tupleslist)
